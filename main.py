e81b42cbeb03c59dfaca0185fea7f7f8ea1f1a9b
Dennis Angemi <dennisangemi@gmail.com>
2023-03-29 19:28:28 +0200

    feat: suppressed imports output, managed new lines, added emoji (#1)

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
+print("👀 Loading...")
+
+# suppress imports output
+import logging
+logging.basicConfig(level=logging.CRITICAL)
+
+# loading libraries
 import os
 from pathlib import Path
 from dotenv import load_dotenv
-
 from llama_index import GPTSimpleVectorIndex, download_loader
 import openai
 
 load_dotenv()
 openai.api_key = os.environ["OPENAI_API_KEY"]
-
 
 def ask(file):
     PDFReader = download_loader("PDFReader")
@@ -16,10 +21,25 @@ def ask(file):
 
     index = GPTSimpleVectorIndex.from_documents(documents)
 
+    # clear the screen
+    os.system("clear")
+
+    print("✅ Ready! Let's start the conversation")
+    print("ℹ️ Press Ctrl+C to exit")
+
     while True:
-        prompt = input("[Prompt]: ")
+        prompt = input("\n😎 Prompt: ")
         response = index.query(prompt)
-        print("[Response]: " + str(response))
+        print()
+
+        # transform response to string
+        response = str(response)
+
+        # if response starts with "\n", remove it
+        if response.startswith("\n"):
+            response = response[1:]
+
+        print("👻 Response: " + str(response))
 
 
 if __name__ == "__main__":
