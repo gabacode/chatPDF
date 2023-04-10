350cd551297a547f8c5710b231373c04949b4346
Gabriele Arcangelo Scalici <gaba41@gmail.com>
2023-04-10 15:33:17 +0200

    feat: add llm_predictor and update libs

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import logging
+
+from langchain import OpenAI
+
 logging.basicConfig(level=logging.CRITICAL)
 
 import os
@@ -6,7 +9,7 @@ from pathlib import Path
 import sys
 
 from dotenv import load_dotenv
-from llama_index import GPTSimpleVectorIndex, download_loader
+from llama_index import GPTSimpleVectorIndex, LLMPredictor, ServiceContext, download_loader
 import openai
 
 
@@ -15,13 +18,16 @@ openai.api_key = os.environ["OPENAI_API_KEY"]
 
 FILES = "./files"
 
+
 def init():
     if not os.path.exists(FILES):
         os.mkdir(FILES)
 
+
 def handle_exit():
     print("\nGoodbye!\n")
     sys.exit(1)
+
 
 def ask(file):
     print("👀 Loading...")
@@ -29,7 +35,10 @@ def ask(file):
     loader = PDFReader()
     documents = loader.load_data(file=Path(file))
 
-    index = GPTSimpleVectorIndex.from_documents(documents)
+    llm_predictor = LLMPredictor(llm=OpenAI(temperature=0, model_name="text-davinci-003"))
+
+    service_context = ServiceContext.from_defaults(llm_predictor=llm_predictor, chunk_size_limit=1024)
+    index = GPTSimpleVectorIndex.from_documents(documents, service_context=service_context)
 
     # clear the screen
     os.system("clear")
@@ -62,25 +71,26 @@ def select_file():
     os.system("clear")
     files = [file for file in os.listdir(FILES) if file.endswith(".pdf")]
     if len(files) == 0:
-        return 'file.pdf' if os.path.exists('file.pdf') else None
+        return "file.pdf" if os.path.exists("file.pdf") else None
     print("📁 Select a file")
     for i, file in enumerate(files):
         print(f"{i+1}. {file}")
     print()
 
     try:
-        possible_selections = [i for i in range(len(files)+1)]
+        possible_selections = [i for i in range(len(files) + 1)]
         selection = int(input("Enter a number, or 0 to exit: "))
         if selection == 0:
             handle_exit()
         elif selection not in possible_selections:
             select_file()
         else:
-            file_path = os.path.abspath(os.path.join(FILES, files[selection-1]))
+            file_path = os.path.abspath(os.path.join(FILES, files[selection - 1]))
     except ValueError:
         select_file()
-    
+
     return file_path
+
 
 if __name__ == "__main__":
     init()
